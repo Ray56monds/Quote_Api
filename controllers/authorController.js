d9ef55488702a5e403afd44a655d9f674598ebc6
Ray56monds <magezir2@stanbic.com>
2024-04-23 18:17:15 +0300

    commit changes in the controllers folder routes folder

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,13 +1,9 @@
 import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcryptjs';
 import dotenv from 'dotenv';
 
 dotenv.config();
 const prisma = new PrismaClient();
 export const getAllAuthors = async (req, res) => {
-   // if (user.role !== 'ADMIN') {
-  //   return res.status(403).json({ message: 'Unauthorized' });
-  // }
   try {
     const authors = await prisma.author.findMany();
     return res.status(200).json(authors);
@@ -36,15 +32,6 @@ export const createAuthor = async (req, res) => {
     const newAuthor = await prisma.author.create({
       data: req.body
     });
-    // const token = jwt.sign(
-    //   {
-    //     id: newAuthor.id,
-    //     email: newAuthor.email,
-    //     role: newAuthor.role,
-    //   },
-    //   process.env.JWT_SECRET,
-    //   { expiresIn: '1h' }
-    // );
     return res.status(201).json({ author: newAuthor });
   } catch (error) {
     return res.status(500).json({ error,message: 'Error creating author' });
@@ -52,33 +39,22 @@ export const createAuthor = async (req, res) => {
 };
 
 export const updateAuthor = async (req, res) => {
-  const { id } = req.params;
-  const { email, password, role } = req.body;
+  const { id } = req.params; // Extract the id from the request parameters
   try {
-    const author = await prisma.author.findUnique({
-      where: { id: Number(id) },
-    });
-    if (!author) {
-      return res.status(404).json({ message: 'Author not found' });
-    }
-    if (email) {
-      author.email = email;
-    }
-    if (password) {
-      author.password = bcrypt.hashSync(password, 8);
-    }
-    if (role) {
-      author.role = role;
-    }
+    // Update the author based on the fields provided in req.body
     const updatedAuthor = await prisma.author.update({
       where: { id: Number(id) },
-      data: author,
+      data: req.body,
     });
+
+    // Return the updated author as JSON
     return res.status(200).json(updatedAuthor);
   } catch (error) {
-    return res.status(500).json({ message: 'Error updating author' });
+    // If an error occurs, return a 500 error along with the error message
+    return res.status(500).json({ error, message: 'Error updating author' });
   }
 };
+
 
 export const deleteAuthor = async (req, res) => {
   const { id } = req.params;
--- routes/quote.route.js
+++ routes/quote.route.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { getAllQuotes, getQuote, createQuote, updateQuote, deleteQuote } from '../controllers/quoteController.js';
 
-const router = express.Router();
+const quoteRoute = express.Router();
 
-router.get("/", getAllQuotes);
-router.get("/:id", getQuote);
-router.get("/", createQuote);
-router.put("/:id", updateQuote);
-router.delete("/:id", deleteQuote);
+quoteRoute.get("/", getAllQuotes);
+quoteRoute.get("/:id", getQuote);
+quoteRoute.get("/", createQuote);
+quoteRoute.put("/:id", updateQuote);
+quoteRoute.delete("/:id", deleteQuote);
 
-export default router
+export default quoteRoute
