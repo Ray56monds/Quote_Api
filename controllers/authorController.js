26705f79a3c339ed5248536b0496947930e430f4
Ray56monds <magezir2@stanbic.com>
2024-04-28 06:52:14 +0300

    commit changes in the controllers folder

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,4 @@
-import { PrismaClient, Role } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { StatusCodes } from 'http-status-codes';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
